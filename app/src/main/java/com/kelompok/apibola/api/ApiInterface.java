b967bf5fbbe3f88ff5fee258c63635a973ed386c
riyanmlg <riyanonoskaciw@gmail.com>
2022-01-08 13:44:26 +0700

    Tugas 8

---
+++ app/src/main/java/com/kelompok/apibola/api/ApiInterface.java
@@ -0,0 +1,23 @@
+package com.kelompok.apibola.api;
+
+import com.kelompok.apibola.models.Responses;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<Responses> getNews (
+            @Query("category") String category,
+            @Query("q") String keyword,
+            @Query("apiKey") String apiKey
+    );
+
+    @GET("top-headlines")
+    Call<Responses> getNewsSearch(
+            @Query("category") String keyword,
+            @Query("language") String language,
+            @Query("apiKey") String apiKey
+    );
+}
